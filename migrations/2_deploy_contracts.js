04aca0cdd3b248bcacd1f528874dfa155ba0fd2d
kellbenet <kb@kaleidochain.io>
2019-11-01 18:21:13 +0800

    add contract TimeLockedPool

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var TimeLockedWalletFactory = artifacts.require("TimeLockedWalletFactory");
+var TimeLockedPool = artifacts.require("TimeLockedPool");
 
 module.exports = function(deployer) {
   deployer.deploy(TimeLockedWalletFactory);
+  deployer.deploy(TimeLockedPool);
 };
